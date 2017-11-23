2d98158a7cfc0e6c18baae3f636c4990e8254334
Pradastur <pradastur@gmail.com>
2017-11-23 16:40:44 +0100

    Update NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,15 +4,29 @@
  * and open the template in the editor.
  */
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {App} from './App';
 
 export class NavBar extends React.Component {
+
+	constructor(props){
+		super(props);
+		this.state={
+			onClick: false
+		}
+		this.changeLogIn=this.changeLogIn.bind(this)
+	}
+
+	changeLogIn() {
+    const newLog = this.state.onClick == false ? true : false;
+    this.setState({ onClick: newLog });
+  }
+
   render() {
   	const pages=['Access']
   	const navLinks= pages.map(page=>{
   		return (
   			<div className='Navigation'>
-  			<button className='NavButton' href={'/' + page}>
+  			<button id="NavButton" className='NavButton' href={'/' + page} onClick={this.changeLogIn} >
   			{page}
   			</button>
   			</div>
