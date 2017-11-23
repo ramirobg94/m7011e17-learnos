e65359296cf1277b06a6b1dcb058ea57ca6dca8d
Pradastur <pradastur@gmail.com>
2017-11-23 16:39:58 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import logo from './logo.svg';
 import './App.css';
 import {NavBar} from './NavBar'
 import {Content} from './Content'
 import {RectangularBox} from './RectangularBox'
+import {LogIn} from './LogIn'
 
-class App extends React.Component {
+export class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state= {
+      login:false
+    }
+  }
+
+  changeContent() {
+    const newContent = this.state.login == false ? true : false;
+    this.setState({ login: newContent });
+  }
+
   render() {
+    if(this.state.login){
     return (
       <div className="App">
         <NavBar /> 
         <div>
-          <Content />
+        <Content />
       </div>
       </div>
     );
   }
+  return (<div className="App">
+        <NavBar/> 
+        <div>
+           <LogIn />
+      </div>
+      </div>
+    );
+}
 }
 
 export default App;
